5891f3c88f10db3c881a7fab1dd7f4935ecdcb86
acap <senieck@gmail.com>
2021-06-04 23:58:01 +0300

    Implement Email form

---
+++ src/components/opt-form/styles/opt-form.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components/macro'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  margin-top: 20px;
+  flex-wrap: wrap;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+export const Input = styled.input`
+  max-width: 420px;
+  width: 100%;
+  border: 0;
+  height: 64px;
+  padding: 8px;
+  box-sizing: border-box;
+`
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  height: 64px;
+  background: #e50914;
+  color: white;
+  text-transform: uppercase;
+  padding: 16px 32px;
+  font-size: 24px;
+  border: 0;
+  cursor: pointer;
+
+  &:hover {
+    background: #f40612;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 16px;
+    height: 48px;
+    margi-top: 20px;
+    font-weight: bold;
+  }
+
+  img {
+    margin-left: 8px;
+    filter: invert(1);
+    width: 24px;
+
+    @media (max-width: 1000px) {
+      width: 16px;
+    }
+  }
+`
+
+export const Text = styled.p`
+  font-size: 20px;
+  color: white;
+  text-align: center;
+
+  @media (max-width: 600px) {
+    font-size: 16px;
+    line-height: 20px;
+  }
+`
+
+export const Break = styled.div`
+  flex-basis: 100%;
+  height: 0;
+`
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,18 +1,24 @@
-import { Accordion } from '../components'
+import { Accordion, OptForm } from '../components'
 import faqData from '../fixtures/faqs.json'
 
-const FaqsContainer = () => {
-  return (
-    <Accordion>
-      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-      {faqData.map(faqItem => (
-        <Accordion.Item key={faqItem.id}>
-          <Accordion.Header>{faqItem.header}</Accordion.Header>
-          <Accordion.Body>{faqItem.body}</Accordion.Body>
-        </Accordion.Item>
-      ))}
-    </Accordion>
-  )
-}
+const FaqsContainer = () => (
+  <Accordion>
+    <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+    {faqData.map(faqItem => (
+      <Accordion.Item key={faqItem.id}>
+        <Accordion.Header>{faqItem.header}</Accordion.Header>
+        <Accordion.Body>{faqItem.body}</Accordion.Body>
+      </Accordion.Item>
+    ))}
+    <OptForm>
+      <OptForm.Input placeholder="Email address" />
+      <OptForm.Button>Try it now!</OptForm.Button>
+      <OptForm.Break />
+      <OptForm.Text>
+        Ready to watch? Enter your email above to create an account!
+      </OptForm.Text>
+    </OptForm>
+  </Accordion>
+)
 
 export default FaqsContainer
