9a862eaac9a9ec451651c4e6196478f976a8af9d
acap <senieck@gmail.com>
2021-06-10 23:58:34 +0300

    Modify App to use router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import {
-  JumbotronContainer,
-  FooterContainer,
-  FaqsContainer,
-} from './containers'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import * as ROUTES from './constants/routes'
+import { Home, SignIn, SignUp } from './routes'
 
 function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+      <Route path={ROUTES.SIGN_IN}>
+        <SignIn />
+      </Route>
+      <Route path={ROUTES.SIGN_UP}>
+        <SignUp />
+      </Route>
+      <Route exact path={ROUTES.BROWSE}></Route>
+    </Router>
   )
 }
 
