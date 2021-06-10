23ecae6f91d7fef031b9e3f6ef55bce2a00adffe
acap <senieck@gmail.com>
2021-06-10 23:59:15 +0300

    Code lint

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -5,14 +5,18 @@ import {
 import { Container, Text, Input, Button, Break } from './styles/opt-form'
 
 const OptForm = createComponent(Container)
+
 OptForm.Input = createComponentChildfree(Input)
+
 OptForm.Button = ({ children, ...restProps }) => (
   <Button {...restProps}>
     {children}
     <img src="/images/icons/chevron-right.png" alt="Try Now" />
   </Button>
 )
+
 OptForm.Text = createComponent(Text)
+
 OptForm.Break = createComponentChildfree(Break)
 
 export default OptForm
